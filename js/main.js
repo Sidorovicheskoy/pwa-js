88de2fe7983c9534215e813ca66624bb980918a2
Bolshevik Remover <87172890+Sidorovicheskoy@users.noreply.github.com>
2025-06-14 14:19:45 +0200

    add notifications

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ window.onload = () => {
  .catch((error) => console.error('Service Worker registration failed:', error));
  }
 };
+
+Notification.requestPermission().then((permission) => {
+  if (permission === "granted") {
+    new Notification("Dzięki za odwiedzenie aplikacji!", {
+      body: "To tylko testowa notyfikacja.",
+      icon: "images/pwa-icon-128.png"
+    });
+  }
+});
